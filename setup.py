7a75e170c60ec43f5a5342fa0fbc9456ed9d4198
Dan Fornika <dfornika@gmail.com>
2022-11-22 16:37:56 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+import illumina_run_monitor
+
+setup(
+    name='illumina-run-monitor',
+    version=illumina_run_monitor.__version__,
+    description='',
+    author='Dan Fornika',
+    author_email='dan.fornika@bccdc.ca',
+    url='https://github.com/BCCDC-PHL/illumina-run-monitor',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    python_requires='>=3.10',
+    install_requires=[
+        "watchdog==2.1.9",
+    ],
+    setup_requires=['pytest-runner', 'flake8'],
+    tests_require=[],
+    entry_points = {
+        'console_scripts': [
+            "monitor-dir = illumina_run_monitor.main:main",
+        ],
+    }
+)
